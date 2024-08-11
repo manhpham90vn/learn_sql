19313b0ecc4b1966263a2580e8da6a58f1afb94f
Manh Pham <manhpham90vn@icloud.com>
2024-08-12 00:05:49 +0700

    update

--- mysql/learn.sql
+++ mysql/learn.sql
@@ -20,21 +20,28 @@ SELECT COUNT(DISTINCT Country) FROM Customers;
 SELECT * FROM Customers WHERE Country='Mexico';
 
 ### WHERE Operators Equal
-SELECT * FROM Products
-WHERE Price = 18;
+SELECT * FROM Products WHERE Price = 18;
 
 ### WHERE Operators Greater than
+SELECT * FROM Products WHERE Price > 30;
 
 ### WHERE Operators Less than
+SELECT * FROM Products WHERE Price < 30;
 
 ### WHERE Operators Greater than or equal
+SELECT * FROM Products WHERE Price >= 30;
 
 ### WHERE Operators Less than or equal
+SELECT * FROM Products WHERE Price <= 30;
 
 ### WHERE Operators Not equal. Note: In some versions of SQL this operator may be written as !=
+SELECT * FROM Products WHERE Price <> 18;
 
 ### WHERE Operators Between a certain range
+SELECT * FROM Products WHERE Price BETWEEN 50 AND 60;
 
 ### WHERE Operators Search for a pattern
+SELECT * FROM Customers WHERE City LIKE 's%';
 
 ### WHERE Operators To specify multiple possible values for a column
+SELECT * FROM Customers WHERE City IN ('Paris','London');
